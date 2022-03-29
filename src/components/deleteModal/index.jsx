ee26a874d651b9a946cbc6d8449651375b778869
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-29 22:01:23 +0700

    add confirmation after delete success

--- src/components/deleteModal/index.jsx
+++ src/components/deleteModal/index.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
-import { DeleteButton, DeleteCard, DeleteTitle, ModalContainer } from "./style";
+import React, { useEffect, useState } from "react";
+import { DeleteButton, DeleteCard, DeleteIcon, DeleteTitle, ModalContainer } from "./style";
+import { MdCheckCircleOutline } from "react-icons/md";
 
 const safeDocument = typeof document !== "undefined" ? document : {};
 
 const DeleteModal = (props) => {
+  const [isDeleted, setIsDeleted] = useState(false);
+
   const handleDelete = () => {
     localStorage.setItem("my_pokemon_data", JSON.stringify([]));
-    onCancel();
+    if (props.pageName !== "pokemon-detail") props.setClearOwned(true);
+    setIsDeleted(true);
   };
 
   const onCancel = () => {
+    setIsDeleted(false);
     props.setVisible(false);
   };
 
@@ -25,19 +30,39 @@ const DeleteModal = (props) => {
   return (
     <>
       <ModalContainer visible={props.visible}>
-        <DeleteCard>
-          <DeleteTitle>
-            <h1>Are you sure you want to delete your pokemon?</h1>
-          </DeleteTitle>
+        {isDeleted ? (
+          <DeleteCard>
+            <DeleteTitle>
+              <h1>All your pokemon have been deleted</h1>
+            </DeleteTitle>
 
-          <DeleteButton>
-            <button className="success" onClick={handleDelete}>Yes</button>
+            <DeleteIcon>
+              <MdCheckCircleOutline size={100} />
+            </DeleteIcon>
 
-            <button className="danger" onClick={onCancel}>
-              Cancel
-            </button>
-          </DeleteButton>
-        </DeleteCard>
+            <DeleteButton>
+              <button className="success" onClick={onCancel}>
+                Ok
+              </button>
+            </DeleteButton>
+          </DeleteCard>
+        ) : (
+          <DeleteCard>
+            <DeleteTitle>
+              <h1>Are you sure you want to delete your pokemon?</h1>
+            </DeleteTitle>
+
+            <DeleteButton>
+              <button className="success" onClick={handleDelete}>
+                Yes
+              </button>
+
+              <button className="danger" onClick={onCancel}>
+                Cancel
+              </button>
+            </DeleteButton>
+          </DeleteCard>
+        )}
       </ModalContainer>
     </>
   );
