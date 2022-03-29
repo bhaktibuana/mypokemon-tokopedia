833d66f5b05ae2c141c2a92b5353bae5eef35bea
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-29 22:03:55 +0700

    init navbar, sidebar, footer on my pokemon list page

--- src/pages/myPokemonList/index.jsx
+++ src/pages/myPokemonList/index.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { SidebarStateContext } from "../../App";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+import Sidebar from "../../components/sidebar";
+import {
+  ContentBody,
+  ContentContainer,
+  MyPokemonContainer,
+  MyPokemonContent,
+} from "./style";
 
 const MyPokemon = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [clearOwned, setClearOwned] = useState(false);
+
   return (
     <>
-      <div>This page is under development.</div>
-    </>
-  )
-}
+      <MyPokemonContainer>
+        <SidebarStateContext.Provider
+          value={[showSidebar, setShowSidebar, setClearOwned]}
+        >
+          <Sidebar pageName="my-pokemon-list" />
+          <Navbar pageName="my-pokemon-list" />
+        </SidebarStateContext.Provider>
 
-export default MyPokemon
+        <MyPokemonContent>
+          <ContentContainer>
+            <ContentBody>
+              <h3>This page is under development</h3>
+            </ContentBody>
+          </ContentContainer>
+
+          <Footer />
+        </MyPokemonContent>
+      </MyPokemonContainer>
+    </>
+  );
+};
+
+export default MyPokemon;
